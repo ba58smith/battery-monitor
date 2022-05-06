4b28acb19bad717a5d14bfd78f771376d7ba7187
ba58smith <ba58smith@gmail.com>
2022-05-06 18:41:35 -0400

    Adjust humidity alarm upper limit

--- src/config.h
+++ src/config.h
@@ -37,7 +37,7 @@ const char *PASSWORD = "sfaesfae";
 #define TEMP_ALARM_RANGE_UPPER 85.0F // s/b 85
 #define PRESSURE_ALARM_RANGE_LOWER 29.0F // inches of hg - normal is 29.0 - 31.0
 #define PRESSURE_ALARM_RANGE_UPPER 31.0F // s/b 31.0
-#define HUMIDITY_ALARM_RANGE_LOWER 30.0F   // s/b 30.0 - 50.0 per Mayo Clinic
-#define HUMIDITY_ALARM_RANGE_UPPER 50.0F
+#define HUMIDITY_ALARM_RANGE_LOWER 30.0F // s/b 30.0 - 50.0 per Mayo Clinic
+#define HUMIDITY_ALARM_RANGE_UPPER 50.0F // 50.0 gave too many false alarms
 
 #endif // _CONFIG_H_
